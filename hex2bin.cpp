d927200d74f7f2c569fbf36f00c3ed8787e1e6c3
progman <gnuplanet@gmail.com>
2016-07-19 17:26:42 +0300

    read speedup

--- hex2bin.cpp
+++ hex2bin.cpp
@@ -1,93 +1,197 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// 0.0.3
+// 0.0.4
 // Alexey Potehin <gnuplanet@gmail.com>, http://www.gnuplanet.ru/doc/cv
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "submodule/libcore.cpp/libcore.hpp"
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define BUFFER_SIZE 65536
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // convert hex2bin
-int hex2bin()
+int hex2bin(size_t size_max)
 {
-	int ch1, ch2;
-	uint8_t r1, r2, out;
+	uint8_t r1;
+	uint8_t r2;
+	uint8_t out;
+	ssize_t size;
+	uint8_t *pin;
+	uint8_t *pin_end;
+
+
+	void *pdata = malloc(size_max);
+	if (pdata == NULL)
+	{
+		printf("ERROR[malloc()]: %s (%u)\n", strerror(errno), errno);
+		return -1;
+	}
 
 
 // read from stdin, convert to bin, write to stdout
 	for (;;)
 	{
-		ch1 = getchar();
-		if (ch1 == EOF) break;
-
-		ch2 = getchar();
-		if (ch2 == EOF) break;
-
-		if (libcore::hex2bin((uint8_t)ch1, r1) == false)
+		size = read(STDIN_FILENO, pdata, size_max); // this faster than getchar()
+		if (size == 0)
 		{
-			return -1;
+			break; // EOF
 		}
-		if (libcore::hex2bin((uint8_t)ch2, r2) == false)
+		if (size == ssize_t(-1))
 		{
+			printf("ERROR[read()]: %s (%u)\n", strerror(errno), errno);
 			return -1;
 		}
 
-		out = (uint8_t)((r1 << 4) + r2);
 
-		putchar(out);
+		pin = (uint8_t *)pdata;
+		pin_end = pin + size;
+		for (;;)
+		{
+			if (pin == pin_end) break;
+
+
+			if (libcore::hex2bin(*pin, r1) == false)
+			{
+				return -1;
+			}
+			pin++;
+
+
+			if (pin == pin_end) break;
+
+
+			if (libcore::hex2bin(*pin, r2) == false)
+			{
+				return -1;
+			}
+			pin++;
+
+
+			out = (uint8_t)((r1 << 4) + r2);
+			putchar(out);
+		}
 	}
+
 	fflush(stdout);
+
+	free(pdata);
 
 	return 0;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // convert bin2hex
-void bin2hex()
+int bin2hex(size_t size_max)
 {
+	ssize_t size;
+	uint8_t *pin;
+	const char *pout;
+	ssize_t i;
+
+
+	void *pdata = malloc(size_max);
+	if (pdata == NULL)
+	{
+		printf("ERROR[malloc()]: %s (%u)\n", strerror(errno), errno);
+		return -1;
+	}
+
+
 // read from stdin, convert to hex, write to stdout
 	for (;;)
 	{
-		int ch = getchar();
-		if (ch == EOF) break;
+		size = read(STDIN_FILENO, pdata, size_max); // this faster than getchar()
+		if (size == 0)
+		{
+			break; // EOF
+		}
+		if (size == ssize_t(-1))
+		{
+			printf("ERROR[read()]: %s (%u)\n", strerror(errno), errno);
+			return -1;
+		}
 
-		const char *p = libcore::bin2hex((uint8_t)ch);
-		putchar(*p);
-		p++;
-		putchar(*p);
+		pin = (uint8_t *)pdata;
+		for (i=0; i < size; i++)
+		{
+			pout = libcore::bin2hex(*pin);
+			pin++;
+
+			putchar(*pout);
+			pout++;
+			putchar(*pout);
+		}
 	}
+
 	fflush(stdout);
+
+	free(pdata);
+
+	return 0;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // convert bin2hex_c
-void bin2hex_c()
+int bin2hex_c(size_t size_max)
 {
-// read from stdin, convert to hex, write to stdout
-
 	bool flag_first = true;
+	ssize_t size;
+	uint8_t *pin;
+	const char *pout;
+	ssize_t i;
+
+
+	void *pdata = malloc(size_max);
+	if (pdata == NULL)
+	{
+		printf("ERROR[malloc()]: %s (%u)\n", strerror(errno), errno);
+		return -1;
+	}
+
+
+// read from stdin, convert to hex, write to stdout
 	for (;;)
 	{
-		int ch = getchar();
-		if (ch == EOF) break;
-
-		const char *p = libcore::bin2hex((uint8_t)ch);
-
-		if (flag_first == false)
+		size = read(STDIN_FILENO, pdata, size_max); // this faster than getchar()
+		if (size == 0)
 		{
-			putchar(',');
-			putchar(' ');
+			break; // EOF
 		}
-		else
+		if (size == ssize_t(-1))
 		{
-			flag_first = false;
+			printf("ERROR[read()]: %s (%u)\n", strerror(errno), errno);
+			return -1;
 		}
-		putchar('0');
-		putchar('x');
 
-		putchar(*p);
-		p++;
-		putchar(*p);
+		pin = (uint8_t *)pdata;
+		for (i=0; i < size; i++)
+		{
+			pout = libcore::bin2hex(*pin);
+			pin++;
+
+			if (flag_first == false)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				flag_first = false;
+			}
+
+			putchar('0');
+			putchar('x');
+
+			putchar(*pout);
+			pout++;
+			putchar(*pout);
+		}
 	}
+
 	fflush(stdout);
+
+	free(pdata);
+
+	return 0;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // general function
@@ -95,7 +199,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-		if (hex2bin() == -1)
+		if (hex2bin(BUFFER_SIZE) == -1)
 		{
 			return 1;
 		}
@@ -104,7 +208,7 @@ int main(int argc, char *argv[])
 
 	if (strcmp(argv[1], "--hex2bin") == 0)
 	{
-		if (hex2bin() == -1)
+		if (hex2bin(BUFFER_SIZE) == -1)
 		{
 			return 1;
 		}
@@ -113,13 +217,19 @@ int main(int argc, char *argv[])
 
 	if (strcmp(argv[1], "--bin2hex") == 0)
 	{
-		bin2hex();
+		if (bin2hex(BUFFER_SIZE) == -1)
+		{
+			return 1;
+		}
 		return 0;
 	}
 
 	if (strcmp(argv[1], "--bin2hex_c") == 0)
 	{
-		bin2hex_c();
+		if (bin2hex_c(BUFFER_SIZE) == -1)
+		{
+			return 1;
+		}
 		return 0;
 	}
 
